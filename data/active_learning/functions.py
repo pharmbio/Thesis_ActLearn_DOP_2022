9747f839e7bf8b29ce79371b6a058dce93cb4b95
Dalia <dalia141503@gmail.com>
2022-02-16 17:25:55 +0100

    Starting implementation

---
+++ data/active_learning/functions.py
@@ -0,0 +1,38 @@
+from pandas import read_excel
+import csv
+import pandas as pd
+
+def load_csv(filename):
+    file = open(filename, "r", encoding="utf-8")
+    experiments = []
+    headers = []
+    reader = csv.reader(file)
+    for i, lines in enumerate(reader):
+        if i == 0:
+            #headers = [e for e in lines[0].split(";")]
+            h = lines[0].split(";")
+            headers.append(h)
+        else:
+            tmp = lines[0].split(";")
+            for j in range(0,len(tmp)):
+                if (j != 0 and j != 1879):
+                    tmp[j] = float(tmp[j])
+                else:
+                    continue
+            #tmp = [float(tmp[j]) for j in range(0,len(tmp)) if (j!=0 and j!=1879)] #This removes two entries, dunno why. 
+            experiments.append(tmp)
+    file.close()
+    return headers, experiments
+
+def column_extraction(data_list,column_number): #columns start counting in 0
+    col = []
+    for i in range(0,len(experiments)):
+        dato = experiments[i][column_number]
+        col.append(dato)
+    return col 
+
+#def bring_data(path_specs, path_covid):
+    # Loading compound library
+#    specs = pd.read_csv('path_specs')
+#    sss = pd.read_csv('path_covid')
+#    return specs, sss
